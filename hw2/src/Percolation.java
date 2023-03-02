16570cac4c9121690338c31cde3d0fadf2eaf482
basebala <chandna@berkeley.edu>
2023-03-02 01:52:52 -0800

    hi

--- hw2/src/Percolation.java
+++ hw2/src/Percolation.java
@@ -1,10 +1,9 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
-import java.lang.Math;
 
 public class Percolation {
     WeightedQuickUnionUF myUnion;
     WeightedQuickUnionUF myUnion2;
-    private boolean [] open;
+    private boolean[] open;
 
     private int gridWidth;
 
@@ -13,70 +12,69 @@ public class Percolation {
     private boolean percolator;
 
     public Percolation(int N) {
-        if (N<=0){
+        if (N <= 0) {
             throw new IllegalArgumentException();
         }
-        myUnion = new WeightedQuickUnionUF(N*N+1);
-        myUnion2 = new WeightedQuickUnionUF(N*N+1);
-        open = new boolean[N*N];
+        myUnion = new WeightedQuickUnionUF(N * N + 1);
+        myUnion2 = new WeightedQuickUnionUF(N * N + 1);
+        open = new boolean[N * N];
         openCount = 0;
         gridWidth = N;
         percolator = false;
-        for(int i = 0; i<N*N; i++){
+        for (int i = 0; i < N * N; i++) {
             open[i] = false;
         }
     }
 
     public void open(int row, int col) {
-        if (row < 0 || row >= gridWidth || col < 0 || col >= gridWidth){
+        if (row < 0 || row >= gridWidth || col < 0 || col >= gridWidth) {
             throw new IndexOutOfBoundsException();
         }
         open[row * gridWidth + col] = true;
         openCount += 1;
-        if(row > 0){
-            if(isOpen(row-1, col)){
+        if (row == 0) {
+            myUnion.union(row * gridWidth + col, open.length);
+        } else if (row == gridWidth - 1) {
+            myUnion2.union(row * gridWidth + col, open.length);
+        }
+        if (row > 0) {
+            if (isOpen(row - 1, col)) {
                 myUnion.union(row * gridWidth + col, (row - 1) * gridWidth + col);
                 myUnion2.union(row * gridWidth + col, (row - 1) * gridWidth + col);
             }
         }
-        if(row < gridWidth - 1){
-            if(isOpen(row + 1, col)){
+        if (row < gridWidth - 1) {
+            if (isOpen(row + 1, col)) {
                 myUnion.union(row * gridWidth + col, (row + 1) * gridWidth + col);
                 myUnion2.union(row * gridWidth + col, (row + 1) * gridWidth + col);
             }
         }
-        if(col > 0){
-            if(isOpen(row, col - 1)){
+        if (col > 0) {
+            if (isOpen(row, col - 1)) {
                 myUnion.union(row * gridWidth + col, row * gridWidth + col - 1);
                 myUnion2.union(row * gridWidth + col, row * gridWidth + col - 1);
             }
         }
-        if(col < gridWidth - 1){
-            if(isOpen(row, col + 1)){
+        if (col < gridWidth - 1) {
+            if (isOpen(row, col + 1)) {
                 myUnion.union(row * gridWidth + col, row * gridWidth + col + 1);
                 myUnion2.union(row * gridWidth + col, row * gridWidth + col + 1);
             }
         }
-        if(isFull(row, col) && isFull2(row, col)){
+        if (isFull(row, col) && isFull2(row, col)) {
             percolator = true;
         }
     }
 
     public boolean isOpen(int row, int col) {
-        if (row < 0 || row >= gridWidth || col < 0 || col >= gridWidth){
+        if (row < 0 || row >= gridWidth || col < 0 || col >= gridWidth) {
             throw new IndexOutOfBoundsException();
-        }
-        if(row == 0){
-            myUnion.union(row * gridWidth + col, open.length);
-        }
-        else if(row == gridWidth - 1){
-            myUnion2.union(row * gridWidth + col, open.length);
         }
         return open[row * gridWidth + col];
     }
 
     public boolean isFull(int row, int col) {
-        if (row < 0 || row >= gridWidth || col < 0 || col >= gridWidth){
+        if (row < 0 || row >= gridWidth || col < 0 || col >= gridWidth) {
             throw new IndexOutOfBoundsException();
         }
         return myUnion.connected(row * gridWidth + col, open.length);
@@ -91,7 +89,7 @@ public class Percolation {
     }
 
     public boolean isFull2(int row, int col) {
-        if (row < 0 || row >= gridWidth || col < 0 || col >= gridWidth){
+        if (row < 0 || row >= gridWidth || col < 0 || col >= gridWidth) {
             throw new IndexOutOfBoundsException();
         }
         return myUnion2.connected(row * gridWidth + col, open.length);
