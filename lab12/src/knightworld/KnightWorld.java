00531e4733ee1bed812a79ee9608ff77c53e29b9
basebala <chandna@berkeley.edu>
2023-04-10 23:58:36 -0700

    hi

--- lab12/src/knightworld/KnightWorld.java
+++ lab12/src/knightworld/KnightWorld.java
@@ -2,6 +2,7 @@ package knightworld;
 
 import tileengine.TERenderer;
 import tileengine.TETile;
+import tileengine.Tileset;
 
 /**
  * Draws a world consisting of knight-move holes.
@@ -9,12 +10,22 @@ import tileengine.TETile;
 public class KnightWorld {
 
     private TETile[][] tiles;
+    private int height;
+    private int width;
     // TODO: Add additional instance variables here
+    // 1, 4, 2, 0, 3
 
     public KnightWorld(int width, int height, int holeSize) {
-        // TODO: Fill in this constructor and class, adding helper methods and/or classes as necessary to draw the
-        //  specified pattern of the given hole size for a window of size width x height. If you're stuck on how to
-        //  begin, look at the provided demo code!
+        tiles = new TETile[width][height];
+        this.width = width;
+        this.height = height;
+        int widthDone = 0;
+        int start = 1;
+        while (widthDone < width) {
+            drawColumn(start, holeSize, widthDone);
+            start = (start + 3) % 5;
+            widthDone += holeSize;
+        }
     }
 
     /** Returns the tiles associated with this KnightWorld. */
@@ -24,9 +35,9 @@ public class KnightWorld {
 
     public static void main(String[] args) {
         // Change these parameters as necessary
-        int width = 50;
-        int height = 30;
-        int holeSize = 2;
+        int width = 60;
+        int height = 40;
+        int holeSize = 3;
 
         KnightWorld knightWorld = new KnightWorld(width, height, holeSize);
 
@@ -34,5 +45,42 @@ public class KnightWorld {
         ter.initialize(width, height);
         ter.renderFrame(knightWorld.getTiles());
 
+    }
+    public void drawFloorTile (int holeSize, int left, int bottom) {
+        int curLeft = left;
+        while(curLeft < left + holeSize && curLeft < width) {
+            int curBottom = bottom;
+            while(curBottom < bottom + holeSize && curBottom < height) {
+                tiles[curLeft][curBottom] = Tileset.LOCKED_DOOR;
+                curBottom += 1;
+            }
+            curLeft += 1;
+        }
+    }
+
+    public void drawHoleTile (int holeSize, int left, int bottom) {
+        int curLeft = left;
+        while(curLeft < left + holeSize && curLeft < width) {
+            int curBottom = bottom;
+            while(curBottom < bottom + holeSize && curBottom < height) {
+                tiles[curLeft][curBottom] = Tileset.NOTHING;
+                curBottom += 1;
+            }
+            curLeft += 1;
+        }
+    }
+    public void drawColumn (int start, int holeSize, int left) {
+        int heightDone = 0;
+        while(heightDone < height) {
+            if (start == 0) {
+                drawHoleTile(holeSize, left, heightDone);
+                start = 4;
+            }
+            else {
+                drawFloorTile(holeSize, left, heightDone);
+                start -=1;
+            }
+            heightDone += holeSize;
+        }
     }
 }
--- lab12/src/project3prep.md
+++ lab12/src/project3prep.md
@@ -2,22 +2,22 @@
 
 **What distinguishes a hallway from a room? How are they similar?**
 
-Answer:
+Answer: A hallway is restricted to a width of 1 (or 2). However, they are both types of rooms, and follow all properties of rooms.
 
 **Can you think of an analogy between the process of 
 drawing a knight world and randomly generating a world 
 using rooms and hallways?**
 
-Answer:
+Answer: The holes for the knight are like rooms, and the floor tiles are like hallways in the sense that you must traverse the path (floor/hallway) to reach the destination (hole/room).
 
 **If you were to start working on world generation, what kind of method would you think of writing first? 
 Think back to the lab and the process used to eventually 
 get to the full knight world.**
 
-Answer:
+Answer: The first method would create the basic grid of tiles, using a blank grid of all Tileset.NOTHING. This would mitigate the risk of null errors.
 
 **This question is open-ended. Write some classes 
 and methods that might be useful for Project 3. Think 
 about what helper methods and classes you used for the lab!**
 
-Answer:
+Answer: We could create a hallway-generating method, as well as a hallway class. Additionally, we need to account for the fact that there will be at least one turning hallway, so we could use a method to check if hallways cross paths.   
